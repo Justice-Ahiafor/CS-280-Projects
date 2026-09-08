96b8fd8d74c23c86f1f5393a30b971769953bc87
Justice-Ahaifor <jahiafor@hcc-nd.edu>
2026-09-08 10:51:13 -0400

    Comparable runs all tests without error and I have modified; Filling array with random numbers, and measuring runtime in all my individual sorting algorithms.

--- src/assignments/sorting/InsertionSort.java
+++ src/assignments/sorting/InsertionSort.java
@@ -10,32 +10,24 @@ public class InsertionSort<T extends Comparable<T>> extends SortingAlgorithm<T> 
      */
 
     public void sort(T[] array) {
-        /* 
-         * Loop begins at index 1 because the element from the first index is already "sorted"
-         */
+        //Loop begins at index 1 because the element from the first index is already "sorted"
         for (int i = 1; i < array.length; i ++) {
             T FirstValue = array[i];
             int j = i - 1;
-
-            /*
-             * Shift elements of all the ones greater than the FirstValue to the right
-             */
+            // Shift elements of all the ones greater than the FirstValue to the right
             while (j >= 0 && array[j].compareTo(FirstValue) > 0) {
                 array[j + 1] = array[j];
                 j = j - 1;                
             }
-            /*
-             * Put back the FirstValue into its correct sorted spot
-             */
+            // Put back the FirstValue into its correct sorted spot
             array[j + 1] = FirstValue;
         }
     }
-    /**
+    /*
      * Puttimg elements one-by-one into a guaranteed sorted list
      * Key features:
      * a. Maintain a sub-array which is always sorted in each iteration.
      * b. A new item has been put in the correct spot.
-     * @param args
      */
 
     /**
@@ -47,18 +39,14 @@ public class InsertionSort<T extends Comparable<T>> extends SortingAlgorithm<T> 
         SortingAlgorithm.validate(new InsertionSort<Integer>());
         System.out.println("InsertionSort has passed all tests.");
 
-        /*
-         * Fill an array with random numbers
-         */
+        // Fill an array with random numbers
         int N = 19999;
         Integer[] array = new Integer[N];
         for (int i = 0; i < array.length; i ++) {
             array[i] = (int)(N*Math.random());
         }
 
-        /* 
-         * Measuring runtime.
-         */
+        // Measuring runtime.
         SortingAlgorithm<Integer> sorter = new InsertionSort<Integer>();
         long start = System.nanoTime();
 
--- src/assignments/sorting/SelectionSort.java
+++ src/assignments/sorting/SelectionSort.java
@@ -13,33 +13,24 @@ public class SelectionSort<T extends Comparable<T>> extends SortingAlgorithm<T> 
         
         for (int k = 0; k < array.length - 1; k ++ ) {
             int SmallestElement = k;        
-            /* 
-             * Assume the smallest element is at index k.
-             */
+            // Assume the smallest element is at index k.
            
             for (int i = k + 1; i < array.length; i ++) {
                 if (array[i].compareTo(array[SmallestElement]) < 0) {
                     SmallestElement = i;    
-                    /*
-                     * Updates the array to the smallest element as it goes.
-                     */
+                    // Updates the array to the smallest element as it goes.
                 }
             }
-            /* 
-             * Swap with the smallest element
-             */
+            // Swap with the smallest element
             swap(array, k, SmallestElement);
         }
 
     }
 
-    /**
+    /*
      * Picks out the smallest element one-by-one
      * a. It identifies the smallest element in the unsorted group
-     * b. Then, you move that element as far to the left of the unsorted group as possible
-     * 
-     * @param array an array of integers
-     * 
+     * b. Then, you move that element as far to the left of the unsorted group as possible    * 
      */
     private void swap(T[] array, int k, int i) {
         T temp = array[k];
@@ -55,18 +46,14 @@ public class SelectionSort<T extends Comparable<T>> extends SortingAlgorithm<T> 
         SortingAlgorithm.validate(new SelectionSort<Integer>());
         System.out.println("SelectionSort has passed all tests.");
 
-        /*
-         * Fill an array with random numbers
-         */
+        // Fill an array with random numbers
         int N = 19999;
         Integer[] array = new Integer[N];
         for (int i = 0; i < array.length; i ++) {
             array[i] = (int)(N*Math.random());
         }
         
-        /*
-         * Measuring runtime.
-         */
+        // Measuring runtime.
         SortingAlgorithm<Integer> sorter = new SelectionSort<Integer>();
         long start = System.nanoTime();
 
