8000ab9d8fbed03ce5d68fa28ee4b43c7e50e404
Justice-Ahaifor <jahiafor@hcc-nd.edu>
2026-09-23 08:48:55 -0400

    CircularLinkedList passes all tests without error

--- src/Test.java
+++ src/Test.java
@@ -38,6 +38,7 @@ public class Test {
         /* Call additional main routines as you create new data strutures. */
         Vector.main(args);
         LinkedList.main(args);
+        CircularLinkedList.main(args);
 
         return true;
     }
